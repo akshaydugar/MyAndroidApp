e0cddaf2df54087fd5fd8407662f69bd25e793f7
zahmed <zarahmed@in.ibm.com>
2016-06-11 22:27:31 +0530

    Basic code for file IO

--- app/src/main/java/com/sample/myandroidapp/MainActivity.java
+++ app/src/main/java/com/sample/myandroidapp/MainActivity.java
@@ -1,15 +1,52 @@
 package com.sample.myandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button handleFileButton;
+    Button makeNetworkRequestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleFileButton = (Button) findViewById(R.id.button_file_op);
+        makeNetworkRequestButton = (Button) findViewById(R.id.button_network_op);
+
+        handleFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                handleFileButtonClick();
+            }
+        });
+
+        makeNetworkRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                handleNetworkRequestButtonClick();
+            }
+        });
+
+    }
+
+    private void handleFileButtonClick()
+    {
+        Intent fileActivityIntent = new Intent(this, FileActivity.class);
+        startActivity(fileActivityIntent);
+
+    }
+
+    private void handleNetworkRequestButtonClick()
+    {
+        Intent networkRequestIntent = new Intent(this, NetworkRequestActivity.class);
+        startActivity(networkRequestIntent);
     }
 }
---
+++ app/src/main/java/com/sample/myandroidapp/NetworkRequestActivity.java
@@ -0,0 +1,10 @@
+package com.sample.myandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zaraahmed on 6/11/16.
+ */
+
+public class NetworkRequestActivity extends AppCompatActivity {
+}
