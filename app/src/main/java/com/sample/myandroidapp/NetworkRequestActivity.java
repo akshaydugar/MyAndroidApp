b6b6f34ebf666090a76745ac845aad6bce8ac87b
Akshay Dugar <aksdugar@in.ibm.com>
2016-06-14 17:08:32 +0530

    Revert "Removing code from network request activity"
    This reverts commit 6cc710a87f85fa14d6383841e1d41843087f3c9f.

--- app/src/main/java/com/sample/myandroidapp/NetworkRequestActivity.java
+++ app/src/main/java/com/sample/myandroidapp/NetworkRequestActivity.java
@@ -1,5 +1,17 @@
 package com.sample.myandroidapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,7 +24,6 @@ import android.widget.TextView;
  */
 
 public class NetworkRequestActivity extends AppCompatActivity {
-
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?";
     private static final String CITY_QUERY_PARAM = "q=";
     private static final String APP_ID_QUERY_PARAM = "&APPID=";
@@ -42,8 +53,52 @@ public class NetworkRequestActivity extends AppCompatActivity {
     }
 
     private void getCurrentWeather() {
-        String cityName = nameOfCityET.getText().toString();
-        // Do network call
+        String cityName = nameOfCityET.getText().toString().trim();
+        new GetWeatherTask(cityName).execute();
     }
 
+    private class GetWeatherTask extends AsyncTask<Void, Void, String> {
+        private final String urlString;
+
+        public GetWeatherTask(String cityName) {
+            urlString = BASE_URL + CITY_QUERY_PARAM + cityName + UNITS_QUERY_PARAM + UNITS_QUERY_VALUE + APP_ID_QUERY_PARAM + API_KEY;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                String response = IOUtils.toString(inputStream);
+                JSONObject jsonResponse = new JSONObject(response);
+
+                JSONArray weatherArray = jsonResponse.getJSONArray("weather");
+                JSONObject weather = weatherArray.getJSONObject(0);
+                String weatherString = weather.getString("main");
+
+                JSONObject main = jsonResponse.getJSONObject("main");
+                String temperatureString = main.getString("temp");
+
+                String result = "Weather: " + weatherString + "\nTemperature: " + temperatureString + "\u2103";
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            resultTV.setText(result);
+        }
+    }
 }
