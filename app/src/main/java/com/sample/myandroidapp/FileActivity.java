46ef2c2994aba17822203c9faafb7e2c3ad4678a
skumarsi <shashikumar@Shashis-MacBook-Pro.local>
2019-01-23 16:58:24 +0530

    testing merge file

--- app/src/main/java/com/sample/myandroidapp/FileActivity.java
+++ app/src/main/java/com/sample/myandroidapp/FileActivity.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 import android.os.Bundle;
+import java.io.*;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
